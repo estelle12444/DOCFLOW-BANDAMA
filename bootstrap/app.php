f31cd8a49b7a2f878c7394797ad9ab496f3dfdc7
Pascale Bandama <bandamapascale@gmail.com>
2025-04-29 19:49:34 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

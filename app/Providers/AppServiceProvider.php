cecd6ce4d2a86c288e83f8b704bd521e727825ca
Pascale Bandama <bandamapascale@gmail.com>
2025-04-30 15:59:58 +0000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Support\Facades\URL;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -24,13 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Illuminate\Support\Facades\Request::setTrustedProxies(
-            [Request::getClientIp()],
-            SymfonyRequest::HEADER_X_FORWARDED_ALL
-        );
-    
-        if (request()->header('x-forwarded-proto') === 'https') {
-            URL::forceScheme('https');
-        }
+        
     }
 }
